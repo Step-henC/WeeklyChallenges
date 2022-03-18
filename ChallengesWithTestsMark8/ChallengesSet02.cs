e70ca1e3b92af8b0e268a028194521b7f9b72e9a
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-18 14:18:33 -0500

    second commit

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,7 +9,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            if (c >= 'A' && c <= 'Z' || c>= 'a' && c<= 'z')
+            if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
             {
                 return true;
             }
@@ -16,47 +18,115 @@ namespace ChallengesWithTestsMark8
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+
+                { count++; }
+            }
+            if (count % 2 == 0)
+            { return true; }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            { return true; }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            { return true; }
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            
+                
+            double min = numbers.Min();
+            double max = numbers.Max();  
+            return min + max;
+            
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            { return str2.Length; }
+            else if (str1.Length < str2.Length)
+            { return str1.Length; }
+            else { return str1.Length; }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+            try
+            {
+                return numbers.Sum(x => x);
+            }
+            catch (ArgumentNullException)
+            { return 0; }
+
+
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers==null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            return numbers.Where(x => x%2 == 0).Sum();
+
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count == 0)
+            {
+                return false;
+
+            }
+            var oddNum = numbers.Sum();
+            if (oddNum%2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+           
+            if (number < 0 || number == 0)
+            {
+                return 0;
+            }
+            long countPosOdd = 0;
+            for (int i = 0; i<number; i++)
+            {
+               if (i > 0 && i%2 != 0)
+                { countPosOdd++; }
+            }
+            return countPosOdd;
         }
     }
+
+    
 }
--- ChallengesWithTestsMark8/Program.cs
+++ ChallengesWithTestsMark8/Program.cs
@@ -7,6 +7,19 @@ namespace ChallengesWithTestsMark8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+
+            var switchedArray = ReverseFirstAndLast(new int[] { 1, 2, 3 });
+        }
+
+        static int[] ReverseFirstAndLast (int[] array)
+        {
+            var firstElement = array[0];
+            var lastElement = array[^1];
+
+            array[0] = lastElement;
+            array[^1] = firstElement;
+
+            return array;
         }
     }
 }
