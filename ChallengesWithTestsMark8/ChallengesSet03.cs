06afa3388d70e0a759c07e33169c423da07b2972
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-02 20:52:22 -0500

    4th commit

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -91,7 +91,7 @@ namespace ChallengesWithTestsMark8
 
         public int LastMinusFirst(int[] nums)
         {
-            return nums[nums.Length - 1] - nums[0];
+            return nums[^1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,119 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                if(numbers[i]%2 == 0)
+                {
+                    sum += numbers[i];
+                }
+                else
+                {
+                    sum -= numbers[i];  
+                }
+                
+            }
+            
+            return sum;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
-        {
-            throw new NotImplementedException();
+        {   
+            
+            List<int> sorting = new List<int>() { str1.Length, str2.Length, str3.Length, str4.Length };
+            sorting.Sort();
+           
+            return sorting.Min();
+            
+               
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> nums = new List<int>() { number1, number2, number3, number4 };
+            nums.Sort();
+            return nums[0];
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 && sideLength1 + sideLength3 > sideLength2;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+           if (input == null)   
+                return false;
+
+            
+            bool numStr = double.TryParse(input, out _);
+            return numStr;
         }
+
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int counter = 0;
+            for(int i = 0; i<objs.Length; i++)
+            {
+                if(objs[i] == null)
+                { counter++; }
+            }
+            if(counter >(objs.Length/2))
+            { return true; }
+            return false;
         }
 
         public double AverageEvens(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {   
+            double evenCount = 0;
+            double sum = 0;    
+            if (numbers == null || numbers.Length == 0)
+            { return 0; }
+           
+            
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        sum += numbers[i];
+                        evenCount++;
+                    }
+
+                }
+                if(evenCount == 0)
+                     { return 0; }
+                return Convert.ToDouble(sum/evenCount);
+           
+         
+            
+              
+            
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            try
+            {   while (Factorial(number) < 1)
+                {
+                    return number * Factorial(number - 1);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            return 0;
+           
         }
     }
 }
